47260efb55fbbf3f2f354ba193058b69797da6ed
ifindev <arifin.muhammad2610@Gmail.com>
2025-09-13 14:44:53 +0700

    feat: add better-auth migrations

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -1,2 +1,3 @@
+export { account, session, user, verification } from "./schemas/auth.schema";
 export { categories } from "./schemas/category.schema";
 export { todos } from "./schemas/todo.schema";
