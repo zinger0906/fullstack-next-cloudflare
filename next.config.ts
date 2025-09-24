81b7bbca0e254adaca86742bed3ed909b31b7e6d
ifindev <arifin.muhammad2610@Gmail.com>
2025-09-24 15:19:35 +0700

    chore: update next.config

--- next.config.ts
+++ next.config.ts
@@ -4,12 +4,13 @@ const nextConfig: NextConfig = {
     /* config options here */
 };
 
-export default nextConfig;
-
 // Only run during `next dev`, not during `next build`
 if (process.argv.includes("dev")) {
-    const { initOpenNextCloudflareForDev } = await import(
-        "@opennextjs/cloudflare"
+    import("@opennextjs/cloudflare").then(
+        ({ initOpenNextCloudflareForDev }) => {
+            initOpenNextCloudflareForDev();
+        },
     );
-    initOpenNextCloudflareForDev();
 }
+
+export default nextConfig;
